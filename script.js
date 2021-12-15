40777ef46c8c48b91dbfa5a662e77783f261ff1c
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-12-14 21:55:04 -0600

    able to add multiple books to library array

--- script.js
+++ script.js
@@ -29,36 +29,49 @@ function showBookForm() {
   });
 }
 
-function gatherFormData(addBookToLib) {
+/*This function accepts two functions as arguments to avoid 
+repeating variables, but separate functionality. 
+It performs three actions: gathers form inputs, adds inputs to library, 
+and resets inputs for future object creation.*/
+function gatherFormData(addBookToLib, resetBookVals) {
   let author = document.querySelector('.author-input');
   let title = document.querySelector('.book-input');
   let pages = document.querySelector('.pages-input');
   let read = document.querySelector('.read-or-not');
   let form = document.querySelector('.form');
 
-  let inputValues = [];
+  let bookVals = [];
   form.addEventListener('submit', e => {
-    //e.preventDefault();
+    e.preventDefault();
 
     let inputs = [author, title, pages, read];
     inputs.forEach(input => {
       if (input == read) {
-        inputValues.push(input.checked);
+        bookVals.push(input.checked);
       } else {
-        inputValues.push(input.value);
+        bookVals.push(input.value);
       }
     });
-    addBookToLib(inputValues);
-    //form.reset();
+    addBookToLib(bookVals);
+    resetBookVals(bookVals);
+    form.reset();
   });
 }
 
-function addBookToLibrary(inputArr) {
-  let newBook = new Book(...inputArr);
+function addBookToLibrary(bookVals) {
+  let newBook = new Book(...bookVals);
   myLibrary.push(newBook);
   console.log(myLibrary)
 }
 
+function resetBookVals(bookVals) {
+  let i = bookVals.length;
+  for (; i > 0; i--) {
+    bookVals.pop();
+  }
+}
+
 showBookForm();
-gatherFormData(addBookToLibrary);
+gatherFormData(addBookToLibrary, resetBookVals);
+
 
