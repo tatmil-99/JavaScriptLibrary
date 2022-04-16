b85d80e98baeb80dd55122c9a3bb224b78e6e70f
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-04-16 15:53:38 -0500

    rearranges code and places function calls above function declarations. Also removes parameters from gatherFormData()

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function Book(author, title, pages, read) {
   this.read = read
 }
 
+showBookForm();
+gatherFormData();
+
+
 function showBookForm() {
   let headElement = document.querySelector('.header');
   let mainElement = document.querySelector('.main');
@@ -19,42 +23,16 @@ function showBookForm() {
     formContainer.style.display = 'flex';
     wrapper.style.opacity = '0.2';
   });
+
   window.addEventListener('click', e => {
-    if (e.target == footElement ||
-        e.target == mainElement ||
-        e.target == headElement) {
-          formContainer.style.display = 'none';
-          wrapper.style.opacity = '1';
+    if (
+      e.target == footElement ||
+      e.target == mainElement ||
+      e.target == headElement
+      ) {
+      formContainer.style.display = 'none';
+      wrapper.style.opacity = '1';
     }
-  });
-}
-
-/*This function accepts two functions as arguments to avoid 
-repeating variables and to separate functionality. 
-It performs four actions: gathers form inputs, calls addBookToLib(), 
-then calls resetBookVals(), and then resets form.*/
-function gatherFormData(addBookToLib, resetBookVals) {
-  let author = document.querySelector('.author-input');
-  let title = document.querySelector('.book-input');
-  let pages = document.querySelector('.pages-input');
-  let read = document.querySelector('.read-or-not');
-  let form = document.querySelector('.form');
-
-  let bookVals = [];
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    let inputs = [author, title, pages, read];
-    inputs.forEach(input => {
-      if (input == read) {
-        bookVals.push(input.checked);
-      } else {
-        bookVals.push(input.value);
-      }
-    });
-    addBookToLib(bookVals);
-    resetBookVals(bookVals);
-    form.reset();
   });
 }
 
@@ -71,7 +49,36 @@ function resetBookVals(bookVals) {
   }
 }
 
-showBookForm();
-gatherFormData(addBookToLibrary, resetBookVals);
+/*This function accepts two functions as arguments to avoid 
+repeating variables and to separate functionality. 
+It performs four actions: gathers form inputs, calls addBookToLib(), 
+then calls resetBookVals(), and then resets form.*/
+function gatherFormData() {
+  let author = document.querySelector('.author-input');
+  let title = document.querySelector('.book-input');
+  let pages = document.querySelector('.pages-input');
+  let read = document.querySelector('.read-or-not');
+  let form = document.querySelector('.form');
+
+  let bookVals = [];
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let inputs = [author, title, pages, read];
+
+    inputs.forEach(input => {
+      if (input == read) {
+        bookVals.push(input.checked);
+      } else {
+        bookVals.push(input.value);
+      }
+    });
+
+    addBookToLibrary(bookVals);
+    resetBookVals(bookVals);
+
+    form.reset();
+  });
+}
 
 
